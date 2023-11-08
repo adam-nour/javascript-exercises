7496ca7b95f59f12a0f77b1e5afd45a52e0b1f1f
adam-nour <adamnour4848@gmail.com>
2023-11-09 02:59:45 +0300

    my version of solutions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+
+    if(Number.isInteger(min) != true || Number.isInteger(max) != true) {
+    return "ERROR"
+    }
+    else if(((min || max)<0)){
+    return "ERROR"
+    }else if(max>min){
+    return ((max-min)+1) * (min + max) / 2;    
+    }else{
+    return ((min-max)+1) * (max + min) / 2; 
+    }
 
 };
 
