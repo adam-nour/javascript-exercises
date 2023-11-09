58c80d83e3aee3596f7b32d0b96f08e3011157dc
adam-nour <adamnour4848@gmail.com>
2023-11-09 19:35:59 +0300

    added function removeFromArray, passed all test cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,40 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
 
-};
+
+// for(let i=0; i < array.length;i++){
+//     let currentIndex = array[i];
+//     if(array[currentIndex]==removeNumber){
+//     newArray = array.slice(currentIndex,1);
+//     return newArray
+
+//     }
+//     else{
+//         currentIndex = currentIndex +1
+//     }
+
+// }
+    // let newArray = [];
+    // for (let i = 0; i < array.length; i++) {
+    //   let currentIndex = array[i];
+    //   if ((currentIndex !== removeNumber || currentIndex !== removeNumber1)) {
+    //     newArray.push(currentIndex);
+    //   }
+    // }
+    // return newArray;
+
+
+
+        let newArray = [];
+        for (let i = 0; i < array.length; i++) {
+          let currentValue = array[i];
+          if (!valuesToRemove.includes(currentValue)) {
+            newArray.push(currentValue);
+          }
+        }
+        return newArray;
+      
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
