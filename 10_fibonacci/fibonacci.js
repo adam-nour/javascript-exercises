a2b3b5095cf3cd5f81f2de3cc137e36e60bf41f9
adam-nour <adamnour4848@gmail.com>
2023-11-22 15:32:45 +0300

    fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciIndex) {
+    array= [0,1];
+if((parseInt(fibonacciIndex)) >0){
+   for( i=2;i<=fibonacciIndex;i++){
+    
+    array.push(array[i-1]+array[i-2]);
+    
+    }
+    }else{
+        return "OOPS"
+    }
+    
+    return array[fibonacciIndex];
 
 };
 
