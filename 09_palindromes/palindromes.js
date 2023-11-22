ce48a03551bd5e1bf97e24070089fb3978b51102
adam-nour <adamnour4848@gmail.com>
2023-11-22 05:38:47 +0300

    solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
+    let filteredString = '';
+  
+    for (let i = 0; i < str.length; i++) {
+        let currentChar = str[i];
+        if (!isNaN(currentChar) || !punctuation.includes(currentChar)) {
+            filteredString += currentChar.toLowerCase(); // Consider case-insensitive comparison for palindromes
+        }
+    }
 
+    // Check for palindrome
+    let reversedString = filteredString.split('').reverse().join('');
+    return filteredString === reversedString;
 };
 
 // Do not edit below this line
