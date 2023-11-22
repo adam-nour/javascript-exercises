114476cb85fb0dc298ae422c62bc4ede8bb8c160
adam-nour <adamnour4848@gmail.com>
2023-11-23 00:04:38 +0300

    getTheTitles solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+array =[];
+for(i=0;i<books.length;i++){
+  
+    array.push(books[i].title); 
+}
+return array;
 };
 
 // Do not edit below this line
