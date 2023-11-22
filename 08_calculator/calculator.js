7800afa2feac413afcbc4940756d36e50dd6179b
adam-nour <adamnour4848@gmail.com>
2023-11-22 04:42:17 +0300

    added power and factorial to calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,12 +14,20 @@ const multiply = function(args) {
   return Number(args.reduce((accumulator, currentValue) => accumulator * currentValue, 1));
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1) {
+    return 1;
+} else {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
 };
 
 // Do not edit below this line
